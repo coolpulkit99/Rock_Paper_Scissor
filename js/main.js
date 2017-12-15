f1d24525fdb3852b0983002c6391f2b24c3722e1
coolpulkit99 <coolpulkit99@gmail.com>
2017-12-15 10:52:41 +0530

    First commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+/**
+ * Created by user on 14-12-2017.
+ */
+var White=function(e){
+    document.body.style.backgroundColor="White";
+};
+var Black=function(e){
+    document.body.style.backgroundColor="Black";
+};
