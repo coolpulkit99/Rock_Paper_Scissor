00afd93e946b0d944f23e58f97ea9b65be824707
coolpulkit99 <coolpulkit99@gmail.com>
2017-12-17 02:38:31 +0530

    Progress...

--- js/main.js
+++ js/main.js
@@ -15,20 +15,37 @@ document.addEventListener('click',function (e) {
 // alert("msg "+e.target.id);
     //Start function
     if(e.target.id=="start")
-    {alert("start");
-    var timer=5;
-        while(timer>0) {
-            setInterval(function () {
-                alert("Hello");
-            }, 1000);
-        timer-=1;
-            document.getElementById("").innerHTML=timer;
-        }
+    {
+        startTimer(5,document.getElementById("timer"));
     }
     if(e.target.id=="rock")
-        alert("rock");
+    {alert("rock");
+        document.getElementById("start").click();}
     if(e.target.id=="paper")
         alert("paper");
     if(e.target.id=="scissor")
         alert("scissor");
 })
+
+function startTimer(duration, display) {
+    var timer = duration, minutes, seconds;
+    setInterval(function () {
+        minutes = parseInt(timer / 60, 10)
+        seconds = parseInt(timer % 60, 10);
+
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.textContent = minutes + ":" + seconds;
+        --timer;
+        if(timer<3){
+            document.getElementById("timer").style.color="Red";
+        }
+        else{
+            document.getElementById("timer").style.color="Black";
+        }
+        if (timer < 0) {
+            timer = 0;
+        }
+    }, 1000);
+}
