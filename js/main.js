249b9b122c61657bb423893c115680c6bb71a0ca
coolpulkit99 <coolpulkit99@gmail.com>
2017-12-16 12:45:46 +0530

    Progress...

--- js/main.js
+++ js/main.js
@@ -3,7 +3,14 @@
  */
 var White=function(e){
     document.body.style.backgroundColor="White";
+document.getElementById("subtitle").style.color="Black";
 };
 var Black=function(e){
     document.body.style.backgroundColor="Black";
+    document.getElementById ("subtitle").style.color="White";
 };
+document.addEventListener('click',function (e) {
+alert("msg"+e.id);
+//     if(=="title")
+//         alert("title pressed");
+})
