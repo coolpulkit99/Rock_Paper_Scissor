b111048536a9d8168efa96cb5e50aece4cdfdd76
coolpulkit99 <coolpulkit99@gmail.com>
2017-12-21 12:37:48 +0530

    Progress...

--- js/main.js
+++ js/main.js
@@ -5,31 +5,43 @@ var White=function(e){
     document.body.style.backgroundColor="White";
     document.getElementById("subtitle").style.color="Black";
     document.getElementById("title").style.color="Black";
+    //document.getElementById("start").click();
 };
 var Black=function(e){
     document.body.style.backgroundColor="Black";
     document.getElementById ("subtitle").style.color="White";
     document.getElementById("title").style.color="White";
 };
+var usermove="none";
 document.addEventListener('click',function (e) {
 // alert("msg "+e.target.id);
     //Start function
     if(e.target.id=="start")
-    {
+    {document.getElementById("startscreen").style.display="none";
+        document.getElementById("selectscreen").style.display="block";
         startTimer(5,document.getElementById("timer"));
+
     }
     if(e.target.id=="rock")
-    {alert("rock");
-        document.getElementById("start").click();}
+    {usermove="rock"}
     if(e.target.id=="paper")
-        alert("paper");
+        usermove="paper"
     if(e.target.id=="scissor")
-        alert("scissor");
+        usermove="scissor";
+    document.getElementById("rock").style.backgroundColor="#add8e6";
+    document.getElementById("paper").style.backgroundColor="#add8e6";
+    document.getElementById("scissor").style.backgroundColor="#add8e6";
+    document.getElementById(""+usermove).style.backgroundColor="Yellow";
 })
 
 function startTimer(duration, display) {
-    var timer = duration, minutes, seconds;
+    var count=0;
+    var timer = duration;
+    var minutes, seconds;
+
+    setTimeout(
     setInterval(function () {
+
         minutes = parseInt(timer / 60, 10)
         seconds = parseInt(timer % 60, 10);
 
@@ -38,14 +50,30 @@ function startTimer(duration, display) {
 
         display.textContent = minutes + ":" + seconds;
         --timer;
-        if(timer<3){
+        if (timer==-1)
+            count+=1;
+        if (count ==1)
+            gotoResult();
+        if(timer<2){
             document.getElementById("timer").style.color="Red";
         }
-        else{
-            document.getElementById("timer").style.color="Black";
+        else if(timer>=2 && timer<4){
+            document.getElementById("timer").style.color="Orange";
+        }else
+        {
+            document.getElementById("timer").style.color="Green";
         }
         if (timer < 0) {
             timer = 0;
+
         }
-    }, 1000);
+    }, 1000)
+,5000)}
+
+
+function gotoResult(){
+alert(""+usermove);
+    document.getElementById("selectscreen").style.display="none";
+    document.getElementById("resultsscreen").style.display="block";
+
 }
