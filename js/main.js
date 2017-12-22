4b92a95447e9342e19a5e1feac56ae754af1364a
coolpulkit99 <coolpulkit99@gmail.com>
2017-12-22 18:43:56 +0530

    Progress...

--- js/main.js
+++ js/main.js
@@ -5,14 +5,19 @@ var White=function(e){
     document.body.style.backgroundColor="White";
     document.getElementById("subtitle").style.color="Black";
     document.getElementById("title").style.color="Black";
+    document.getElementById("playerdisplay").style.color="Black";
+    document.getElementById("compdisplay").style.color="Black";
     //document.getElementById("start").click();
 };
 var Black=function(e){
     document.body.style.backgroundColor="Black";
     document.getElementById ("subtitle").style.color="White";
     document.getElementById("title").style.color="White";
+    document.getElementById("playerdisplay").style.color="White";
+    document.getElementById("compdisplay").style.color="White";
 };
 var usermove="none";
+var compmove="none";
 document.addEventListener('click',function (e) {
 // alert("msg "+e.target.id);
     //Start function
@@ -23,9 +28,9 @@ document.addEventListener('click',function (e) {
 
     }
     if(e.target.id=="rock")
-    {usermove="rock"}
+    {usermove="rock";}
     if(e.target.id=="paper")
-        usermove="paper"
+        usermove="paper";
     if(e.target.id=="scissor")
         usermove="scissor";
     document.getElementById("rock").style.backgroundColor="#add8e6";
@@ -72,8 +77,46 @@ function startTimer(duration, display) {
 
 
 function gotoResult(){
-alert(""+usermove);
+// alert(""+usermove);
     document.getElementById("selectscreen").style.display="none";
-    document.getElementById("resultsscreen").style.display="block";
+    if(usermove!="none")
+    {
+        document.getElementById("resultsscreen").style.display="block";
+    // var
+        compmove=Math.floor(Math.random() * 3);
+    if(compmove=="0")
+    compmove="rock";
+else
+    if(compmove=="1")
+    compmove="paper";
+else
+    if(compmove=="2")
+        compmove="scissor";
+
+
+    alert(compmove+"");
+document.getElementById("usermove").className+=" "+usermove;
+        document.getElementById("compmove").className+=" "+compmove;
+    if(usermove==compmove)
+    {
+        // Draw
+        alert("Draw");
+    }
+    else if((usermove=="scissor" && compmove=="paper") || (usermove=="paper" && compmove=="rock") || (usermove=="rock" && compmove=="scissor"))
+    {
+        // win
+        alert("Win");
+    }
+    else if((compmove=="scissor" && usermove=="paper") || (compmove=="paper" && usermove=="rock") || (compmove=="rock" && usermove=="scissor"))
+    {
+        // lose
+        alert("Lose");
+    }
+
+    }
+
+    else
+        if(usermove=="none")
+            document.getElementById("foulscreen").style.display="block";
 
 }
